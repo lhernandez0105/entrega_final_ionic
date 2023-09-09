e00af4b48e77e7fba80c577dd9f7bc15a9634010
luishernandez0105 <luisfelipegilhernandez@gmail.com>
2023-09-08 21:12:37 -0500

    infinity scroll

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+  characters: any[] = [];
+  params = {} as any;
+
+
+  constructor(private rymSyc: RickAndMortyService) { }
+
+  ngOnInit() {
+    this.params.page = 0;
+    this.getCharacters();
+  }
+
+  getCharacters(event?: any) {
+    this.params.page += 1;
+    this.rymSyc.getCharacters(this.params).subscribe({
+      next: (respuesta: any) => {
+        console.log(respuesta);
+        this.characters.push(...respuesta.results);
+        console.log(this.characters);
+        if (event){
+          event.target.complete();
+        }
+      },
+      error: (err: any) => { 
+        if (event){
+          event.target.complete();
+        }
+        console.log(err) }
+    });
+  }
+
+}
